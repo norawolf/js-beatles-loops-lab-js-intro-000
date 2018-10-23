cd1c4362ed5f04bffc80f1dfde95c0431cc7abc6
Nora Vogt <noraevogt@gmail.com>
2018-10-23 15:16:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function johnLennonFacts() {
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
-    "He hated the sound of his own voice",
-    "foo",
-    "bar"
+    "He hated the sound of his own voice"
   ];
 
 
