b35994ffa0bd5b1cb5c09246c8278e53a59880b7
Nora Vogt <noraevogt@gmail.com>
2018-10-23 15:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+
+//for ([initialization]; [condition]; [iteration]) {
+//  [loopBody]; }
+
+  for (let i = 0; i <= 3; i ++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return array;
+}
+
+function johnLennonFacts() {
+  var array = [];
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+
+  // while ([condition]) {
+  // [loopBody];
+
+let i = 0;
+  while (i >= 0 && i <=3){
+    array.push(`${facts[i]}!!!`);
+    
+  }
+
+return array;
+
+}
