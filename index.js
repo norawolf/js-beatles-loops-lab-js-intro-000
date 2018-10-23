211c20705c3e1c22ee834bf525e8b8300b31b181
Nora Vogt <noraevogt@gmail.com>
2018-10-23 15:39:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var array = [];
-  //const facts = [
-  //  "He was the last Beatle to learn to drive",
-  //  "He was never a vegetarian",
-  //  "He was a choir boy and boy scout",
-  //  "He hated the sound of his own voice"
-  // ];
 
 let i = 0;
   while (i < facts.length){
